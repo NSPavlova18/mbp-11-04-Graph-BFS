06fc1829609f9a246305b41eca17098352f794f1
Nevena Pavlova <npavlova2004@gmail.com>
2021-12-02 13:02:29 +0200

    new

--- Partial Breadth first search algorithm.cpp
+++ Partial Breadth first search algorithm.cpp
@@ -1,33 +1,40 @@
-void Graph::BFS(int startingVertex) 
+void Graph::BFS(int startingVertex)
 {
-  visited = new bool[numberOfVertices];
-
-  //Initialize visited pointer with false
-
-  list<int> queue;
-
-  visited[startingVertex] = true;
-
-  //Add the starting vertex to the queue
-
-  //define iterator of type list<int>
-
-  while (!queue.empty()) 
-  {
-    int currentVertex = queue.front();
-    cout << "Visited V" << currentVertex << " ";
-    queue.pop_front();
-
-    //For loop definition starting from current vertex to last vertex
+    visited = new bool[numberOfVertices];
+ 
+    //Initialize visited pointer with false
+    for (int i = 0; i < numberOfVertices; i++)
     {
-      int adjacentVertex = *listIterator;
-
-      if (!visited[adjacentVertex]) {
-
-        visited[adjacentVertex] = true;
-
-        queue.push_back(adjacentVertex);
-      }
+        visited[i] = false;
     }
-  }
+ 
+    std::list<int> queue;
+ 
+    visited[startingVertex] = true;
+ 
+    //Add the starting vertex to the queue
+    queue.push_back(startingVertex);
+ 
+    //define iterator of type list<int>
+    std::list<int>::iterator i;
+ 
+    while (!queue.empty())
+    {
+        int currentVertex = queue.front();
+        std::cout << "Visited V" << currentVertex << " ";
+        queue.pop_front();
+ 
+        //For loop definition starting from current vertex to last vertex
+        for (auto i = adjacencyLists[currentVertex].begin(); i != adjacencyLists[currentVertex].end(); ++i)
+        {
+            int adjacentVertex = *i;
+ 
+            if (!visited[adjacentVertex]) {
+ 
+                visited[adjacentVertex] = true;
+ 
+                queue.push_back(adjacentVertex);
+            }
+        }
+    }
 }
